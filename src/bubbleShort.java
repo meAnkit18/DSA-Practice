ca7248a2cf4f2b36f86be1188699445390b37f69
ANKIT KUMAR <ankit1842kumar@gmail.com>
2024-12-07 18:56:04 +0530

    Update and rename bubbleShort to bubbleShort.java

--- src/bubbleShort.java
+++ src/bubbleShort.java
@@ -1,7 +1,15 @@
-//function for bubble short
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
+public class bubble {
+    public static void main(String[] args) {
+        int arr[]={1,6,4,-4,-33,0,3,8,7};
+        System.out.println(Arrays.toString(arr));
+        bubble(arr);
+        
 
-static void bubble(int arr[]){
+    }
+    static void bubble(int arr[]){
         int n=arr.length-1;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 1; j < arr.length; j++) {
@@ -14,3 +22,5 @@ static void bubble(int arr[]){
         }
         System.out.println(Arrays.toString(arr));
     }
+    
+}
