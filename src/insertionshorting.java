e6855f161a21dd8fd25bbfd0506a62ef8ddb4798
ANKIT KUMAR <ankit1842kumar@gmail.com>
2024-12-08 14:08:17 +0530

    Add files via upload

---
+++ src/insertionshorting.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class insertionshorting {
+    public static void main(String[] args) {
+        int arr[]={5,3,2,4,1};
+        insertion( arr);
+    }
+    static void insertion(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j >0 ; j--) {
+                if (arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }else {
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
